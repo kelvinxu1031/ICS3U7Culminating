3a9387ee91beec7e00e87a07ad9c06a34983dd63
kelvinxu1031 <106162687+kelvinxu1031@users.noreply.github.com>
2022-06-15 08:56:12 -0400

    fixed erorrs in how to play page for pac man

--- src/PacManHowToPlay.java
+++ src/PacManHowToPlay.java
@@ -11,33 +11,33 @@ public class PacManHowToPlay extends JFrame implements ActionListener{
 	private JTextArea text;
 	private JButton back;
 	private JButton play;
-	
+
 	private String file = "pacManInstructions.txt";
-	
+
 	String titleFontName = "fonts/titleFont.ttf";
 	String textFontName  = "fonts/textFont.ttf";
-	
+
 	public PacManHowToPlay(String title) throws Exception{
 		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 		Font textFont = Font.createFont(Font.TRUETYPE_FONT, new File(textFontName)).deriveFont(12f);
 		ge.registerFont(textFont);
 		Font titleFont = Font.createFont(Font.TRUETYPE_FONT, new File(titleFontName)).deriveFont(28f);
 		ge.registerFont(titleFont);
-		
+
 		f = new JFrame(title);
 		backgroundP = new JPanel();
 		lblTitle = new JLabel("PAC_MAN");
 		text = new JTextArea();
 		back = new JButton("BACK");
 		play = new JButton("PLAY");
-		
+
 		BufferedReader in = new BufferedReader(new FileReader(file));
 		line = in.readLine();
 		while (line!=null) {
 			text.append(line + "\n");
 			line = in.readLine();
 		}
-		
+
 		createButton(play);
 		createButton(back);
 		f.setLayout(null);
@@ -50,13 +50,13 @@ public class PacManHowToPlay extends JFrame implements ActionListener{
 		text.setBounds(100,125,520,200);
 		back.setBounds(30,360,320,50);
 		play.setBounds(370,360,320,50);
-		
+
 		backgroundP.add(lblTitle);
 		backgroundP.add(text);
 		backgroundP.add(back);
 		backgroundP.add(play);
 		f.add(backgroundP);
-		
+
 		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		f.setLocationRelativeTo(null);
 		f.setVisible(true);
@@ -73,13 +73,17 @@ public class PacManHowToPlay extends JFrame implements ActionListener{
 		b.addActionListener(this);
 		b.setFont(font);
 	}
-	public void ActionPerformed(ActionEvent e) throws Exception {
-		if(e.getSource()==back) {
-			f.dispose();
-			new GameOption("PLAY");
-		}
-		if(e.getSource()==play) {
-			f.dispose();//add new PacMan() when we make the program;
+	public void actionPerformed(ActionEvent e) {
+		try {
+			if(e.getSource()==back) {
+				f.dispose();
+				new GameOption("PLAY");
+			}
+			if(e.getSource()==play) {
+				f.dispose();//add new PacMan() when we make the program;
+			}
+		}catch(Exception e1) {
+			System.out.println("ERROR");
 		}
 	}
 }
