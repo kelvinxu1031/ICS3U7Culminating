99cff64bf2a7f8f1ff5ad6bac629e05e3207efd2
kelvinxu1031 <106162687+kelvinxu1031@users.noreply.github.com>
2022-06-16 09:43:33 -0400

    added run.bat

---
+++ src/run.bat
@@ -0,0 +1,18 @@
+javac Main.java
+javac Login.java
+javac CreateAcc.java
+javac MainMenu.java
+javac ResetPass.java
+javac TitlePage.java
+javac AsteroidsHowToPlay.java
+javac AstronautGame.java
+javac DinosaurGameHowToPlay.java
+javac FlappyBirdHowToPlay.java
+javac GameOver.java
+javac GameOption.java
+javac Leaderboard.java
+javac PacManHowToPlay.java
+javac Stats.java
+
+pause
+java Main
